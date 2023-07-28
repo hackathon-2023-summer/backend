7f724edd45bcda1b04a5240b0f3eb7ffef38954c
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-07-28 23:50:02 +0900

    delete email

--- server/models.py
+++ server/models.py
@@ -8,4 +8,3 @@ class TestUser(Base):
     __tablename__ = 'test_users'
     id = Column(Integer, Sequence('test_user_id_seq'), primary_key=True)
     user_name = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False)
