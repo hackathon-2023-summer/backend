27013f2e828b0b9e2e3919f1c635b42af0535001
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-08-11 14:52:31 +0900

    debug about uvicorn down due to extend_existing

--- server/schemas.py
+++ server/schemas.py
@@ -1,24 +1,28 @@
 from pydantic import BaseModel
-import Date 
+from datetime import date as PythonDate
+
 
 # ユーザーを作るためのデータ構造
 class UserCreate(BaseModel):
     username: str
     password: str
     email: str
-    
+
+
 # ユーザー全体のデータ構造
 class User(UserCreate):
     id: int
 
+
 # レシピを保存するためのデータ構造
 class RecipeCreate(BaseModel):
-    date: Date
+    date: PythonDate
     recipename: str
     category: str
     photo: str
-    is_favorite:bool
+    is_favorite: bool
+
 
 # レシピ全体のデータ構造
 class Recipe(RecipeCreate):
-    recipe_id: int
+    id: int
